bc4cb74b4c3bcd6e57e4d8ca1cd212f1a1dd5623
unknown <Namdnitbk92@gmail.com>
2017-04-09 03:12:17 +0700

    init project

---
+++ app/Status.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+	protected $table = "status";
+
+	 protected $fillable = [
+        'status_id', 'description',
+    ];
+
+    public function places()
+    {
+        return $this->belongsTo('App\Places');
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,262 @@
+<?php
+
+use App\User;
+use App\News;
+use App\Places;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+function baseResponse($statusCode, $message)
+{
+	return [
+		'statusCode' => $statusCode,
+		'message' => $message,
+	];
+}
+
+function guardWeb()
+{
+	return Auth::guard('web');
+}
+
+function getValidator($request, $rules)
+{
+	return Validator::make($request->all(), $rules);
+}
+
+Route::post('/login', function (Request $request) {
+	$validator = getValidator($request, [
+		'email' => 'required',
+		'password' => 'required',
+	]);
+
+	if ($validator->fails()) 
+	{
+		return response()->json(baseResponse(200, 'user email or password is required'));
+	}
+
+	$guard = guardWeb();
+	$isLogged = $guard->attempt($request->only('email', 'password'));
+
+	if ($isLogged)
+	{
+		session()->regenerate();
+
+		return response()->json(
+			array_merge(
+				baseResponse(200, 'Login sucessfully'),
+				[
+					'api_token' => $guard->user()->api_token,
+					'current_user' => $guard->user(),
+				]
+			)
+		);
+	} else
+	{
+		return response()->json(baseResponse(401, 'user email of password is not correct'));
+	}
+});	
+
+Route::post('/logout', function (Request $request) {
+	$api_token = $request->has('api_token') ? $request->get('api_token') : null;
+
+	if (is_null($api_token))
+	{
+		return response()->json(baseResponse(500, 'logout failed, require pass api_token'));
+	}
+
+	$user = User::where('api_token', $api_token)->first();
+	
+	try 
+	{
+		DB::beginTransaction();
+		$user->api_token = str_random(60);
+		$user->save();
+	}
+	catch(Exception $e)
+	{
+		$isFailed = true;
+		DB::rollBack();
+		return response()->json(baseResponse(200, 'logout errors cause:'.$e->getMessage()));
+	}
+
+	if (!isset($isFailed))
+	{	
+		DB::commit();
+	}
+
+	return response()->json(baseResponse(200, 'logout successfully'));
+});
+
+Route::post('register', function (Request $request) {
+	$validator = getValidator($request, [
+		'email' => 'required|email',
+		'password' => 'required|min:6|max:20',
+		'password_confirmation' => 'required|min:6|max:20',
+	]);
+
+	if ($validator->fails()) 
+	{
+		return response()->json(baseResponse(401, 'data is not matching with required'));
+	}
+
+	if ($request['password'] !== $request['password_confirmation'])
+	{
+		return response()->json(baseResponse(401, 'password is not matched with password confirmation'));
+	}
+
+	
+
+	try 
+	{
+		DB::beginTransaction();
+		User::create([
+			'email' => $request->get('email'),
+			'password' => bcrypt($request->get('password')),
+			'api_token' => str_random(60),
+			'name' => $request->get('name'),
+		]);
+	}
+	catch(Exception $e)
+	{
+		$isFailed = true;
+		DB::rollBack();
+		return response()->json(baseResponse(200, 'register user error cause :'.$e->getMessage()));
+	}
+
+	if (!isset($isFailed))
+	{	
+		DB::commit();
+	}
+
+	return response()->json(baseResponse(200, 'register successfully'));
+
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
+	Route::get('/user', function (Request $request) {
+	    return $request->user();
+	});
+
+	Route::get('/getNewsList', function (Request $request) {
+		return response()->json(
+			array_merge(
+				baseResponse(200, 'get news lists successfully'),
+				['news_list' => DB::table('news')->latest()->get()]
+			)
+		); 
+	});
+
+	Route::post('/storePlaceByUser', function (Request $request) {
+		$validator = getValidator($request, [
+			'lat' => 'required',
+			'lng' => 'required',
+			'user_id' => 'required|integer',
+		]);
+
+		if ($validator->fails()) 
+		{
+			return response()->json(baseResponse(401, 'data is not matching with required'));
+		}
+
+		try 
+		{
+			DB::beginTransaction();
+			$places = Places::create($request->all());
+		}
+		catch (Exception $e)
+		{
+			DB::rollBack();
+
+			return response()->json(baseResponse(200, 'store places of customer is errors cause :'.$e->getMessage())); 
+		}
+
+		DB::commit();
+
+		return response()->json(baseResponse(200, 'store places of customer is successfully'));
+	});
+
+	Route::get('/getNewDetail', function (Request $request) {
+		$validator = getValidator($request, [
+			'new_id' => 'required|integer',
+		]);
+
+		if ($validator->fails()) 
+		{
+			return response()->json(baseResponse(401, 'id must be a integer and required'));
+		}
+
+		$new = News::where('id', $request->get('new_id'))->first();
+		$place = $new->places()->get();
+		$user = $new->users()->get();
+
+		return response()->json(
+			array_merge(
+				baseResponse(200, (!is_null($new) || !$new->isEmpty()) ? 'get a new detailed informations successfully' : 'the new with this id is not exists in database'),
+				[
+					'new' => $new,
+					'user' => $user,
+					'place' => $place, 
+				]
+			)
+		); 
+	});
+
+	Route::get('/getNewListByUser', function (Request $request) {
+		$validator = getValidator($request, [
+			'user_id' => 'required|integer',
+		]);
+
+		if ($validator->fails()) 
+		{
+			return response()->json(baseResponse(401, 'id must be a integer and required'));
+		}
+
+		$new = News::where('user_id', $request->get('user_id'))->orderBy('created_at', 'desc')->get();
+
+		return response()->json(
+			array_merge(
+				baseResponse(200, (!is_null($new) || !$new->isEmpty()) ? 'get a new detailed informations by user'. $request->get('user_id') .' successfully' : 'the new created by this user is not exists in database'),
+				['new_data' => $new]
+			)
+		); 
+	});
+
+	Route::get('/getNewListByPlace', function (Request $request) {
+		$validator = getValidator($request, [
+			'type' => 'required|string',
+		]);
+
+		if ($validator->fails()) 
+		{
+			return response()->json(baseResponse(401, 'id must be a integer and required'));
+		}
+
+		$places = Places::where('type', $request->get('type'))->get();
+		$news = [];
+		foreach ($places as $place) {
+			array_push($news, $place->place_id);
+		}
+
+		$news = News::whereIn('id', $news)->get();
+
+		return response()->json(
+			array_merge(
+				baseResponse(200, (!is_null($news) || !$news->isEmpty()) ? 'get a news list informations by places successfully' : 'empty news'),
+				['new_data' => $news]
+			)
+		); 
+	});
+});
+
+
